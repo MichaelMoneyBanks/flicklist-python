df05ef91ac720708afce6e2f9bf92a145d71ec56
Mike Eubanks <mooneyoak@gmail.com>
2017-03-20 18:58:03 -0500

    Made my first web app

---
+++ lc101/flicklist-python/main.py
@@ -0,0 +1,31 @@
+import webapp2
+import random
+
+class Index(webapp2.RequestHandler):
+
+    def getRandomMovie(self):
+
+        movielist = ["Shawn of the Dead", "Ferris Bueller's Day Off", "Interstellar", "Batman Begins", "Logan"]
+
+        return random.choice(movielist)
+
+    def get(self):
+        # choose a movie by invoking our new function
+        movie = self.getRandomMovie()
+
+        # build the response string
+        content = "<h1>Movie of the Day</h1>"
+        content += "<p>" + movie + "</p>"
+
+        self.response.write(content)
+
+        movie2 = self.getRandomMovie()
+
+        # build the response string
+        content = "<h1>Tomorrow's Movie</h1>"
+        content += "<p>" + movie2 + "</p>"
+        self.response.write(content)
+
+app = webapp2.WSGIApplication([
+    ('/', Index)
+], debug=True)
