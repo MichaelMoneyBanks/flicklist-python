86d1c570b1e7e4be8030520798e2aef076cc12e7
Mike Eubanks <mooneyoak@gmail.com>
2017-03-20 19:35:01 -0500

    Updated

--- lc101/flicklist-python/main.py
+++ lc101/flicklist-python/main.py
@@ -4,9 +4,7 @@ import random
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
-
         movielist = ["Shawn of the Dead", "Ferris Bueller's Day Off", "Interstellar", "Batman Begins", "Logan"]
-
         return random.choice(movielist)
 
     def get(self):
@@ -20,11 +18,17 @@ class Index(webapp2.RequestHandler):
         self.response.write(content)
 
         movie2 = self.getRandomMovie()
-
+        if movie != movie2:
         # build the response string
-        content = "<h1>Tomorrow's Movie</h1>"
-        content += "<p>" + movie2 + "</p>"
-        self.response.write(content)
+            content = "<h1>Tomorrow's Movie</h1>"
+            content += "<p>" + movie2 + "</p>"
+            self.response.write(content)
+        else:
+            movie2 = self.getRandomMovie()
+            content = "<h1>Tomorrow's Movie</h1>"
+            content += "<p>" + movie2 + "</p>"
+            self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', Index)
